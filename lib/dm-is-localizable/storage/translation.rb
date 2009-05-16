d1c0a25186673e938e0a6e42979d441a4c8dd5a6
snusnu <gamsnjaga@gmail.com>
2009-05-16 05:23:01 +0200

    proper namespace for module Translation

--- lib/dm-is-localizable/storage/translation.rb
+++ lib/dm-is-localizable/storage/translation.rb
@@ -1,10 +1,18 @@
-module Translation
+module DataMapper
+  module Is 
+    module Localizable
+      
+      module Translation
   
-  include DataMapper::Resource
+        include DataMapper::Resource
   
-  is :remixable
+        is :remixable
   
-  property :id,          Serial
-  property :language_id, Integer, :nullable => false
+        property :id,          Serial
+        property :language_id, Integer, :nullable => false
   
+      end
+      
+    end
+  end
 end
