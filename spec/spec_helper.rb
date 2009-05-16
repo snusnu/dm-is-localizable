cb462f4cb93641bf967c9f09455d04e9ca52d609
snusnu <gamsnjaga@gmail.com>
2009-05-16 19:13:57 +0200

    added support for nice html output in rspec tmbundle.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,18 +29,42 @@ def setup_adapter(name, default_uri = nil)
   end
 end
 
-ENV['ADAPTER'] ||= 'sqlite3'
-setup_adapter(:default)
-Dir[Pathname(__FILE__).dirname.to_s + "/fixtures/**/*.rb"].each { |rb| require(rb) }
-
 # have the loggers handy
 # DataObjects::Logger.new(STDOUT, :debug)
 # DataObjects::Sqlite3.logger = DataObjects::Logger.new(STDOUT, :debug)
 
+# -----------------------------------------------
+# support for nice html output in rspec tmbundle
+# -----------------------------------------------
+
+USE_TEXTMATE_RSPEC_BUNDLE = true # set to false if not using textmate
+
+if USE_TEXTMATE_RSPEC_BUNDLE
+
+  require Pathname(__FILE__).dirname.expand_path + 'shared/rspec_tmbundle_support'
+
+  # use the tmbundle logger
+  RSpecTmBundleHelpers::TextmateRspecLogger.new(STDOUT, :off)
+  
+
+  class Object
+    include RSpecTmBundleHelpers
+  end
+
+end
+
+ENV['ADAPTER'] ||= 'sqlite3'
+setup_adapter(:default)
+Dir[Pathname(__FILE__).dirname.to_s + "/fixtures/**/*.rb"].each { |rb| require(rb) }
+
 
 Spec::Runner.configure do |config|
   
-  config.before do
+  config.before(:all) do
+    DataMapper.auto_migrate!
+  end
+  
+  config.before(:each) do
     DataMapper.repository(:default) do |r|
       transaction = DataMapper::Transaction.new(r)
       transaction.begin
@@ -48,7 +72,7 @@ Spec::Runner.configure do |config|
     end
   end
   
-  config.after do
+  config.after(:each) do
     DataMapper.repository(:default) do |r|
       adapter = r.adapter
       while adapter.current_transaction
