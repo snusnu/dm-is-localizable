30e7f81b7bf7d06241a117f61725eb6a184f9c4c
snusnu <gamsnjaga@gmail.com>
2009-05-18 17:39:08 +0200

    added more specs for the remixed translation class.
    Although the specs for the generated belongs_to association readers
    pass, it's not possible to use :language as query condition when
    calling translations.first :language => 'en-US'. Need to use
    :language_id here. I've no idea why, but it could be some problem
    with dm-is-remixable's enhance method, and association generation
    inside this block.
    
    TODO investigate this!

--- spec/unit/translation_spec.rb
+++ spec/unit/translation_spec.rb
@@ -1,16 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "the remixed translation resource" do
+  
+  before :each do
+    @l = Language.create :code => 'en-US', :name => 'English'
+    @i = Item.create
+    @t1 = ItemTranslation.create(:item => @i, :language => @l)
+  end
+  
+  it "should belong to a localizable resource" do
+    @t1.item.should == @i
+  end
+  
+  it "should belong to a language" do
+    @t1.language.should == @l
+  end
 
   it "should store unique languages for every resource to translate" do
-    l = Language.create :code => 'en-US', :name => 'English'
-    i = Item.create
-    t1 = ItemTranslation.create(:item => i, :language => l)
-    t1.should_not be_new_record
-    t2 = ItemTranslation.create(:item => i, :language => l)
-    t2.should     be_new_record
-    t2.errors.should_not be_empty
-    t2.errors.size.should == 1
+    @t2 = ItemTranslation.create(:item => @i, :language => @l)
+    @t1.should_not be_new_record
+    @t2.should     be_new_record
+    @t2.errors.should_not be_empty
+    @t2.errors.size.should == 1
   end
 
 end
