d735bed1dd0ab8875e81e2cdecda656289799d2b
snusnu <gamsnjaga@gmail.com>
2011-04-04 13:43:33 +0200

    Encapsulate naming logic in Localizer::NamingConvention

--- lib/dm-is-localizable/model.rb
+++ lib/dm-is-localizable/model.rb
@@ -52,51 +52,59 @@ module DataMapper
 
       class Localizer
 
+        class NamingConvention
+
+          attr_reader :remixer_fk
+          attr_reader :remixer
+          attr_reader :remixee
+
+          def initialize(model, options)
+            fk_string   = DataMapper::Inflector.foreign_key(model.name)
+            @remixer_fk = fk_string.to_sym
+            @remixer    = fk_string[0, fk_string.rindex('_id')].to_sym
+            demodulized = DataMapper::Inflector.demodulize(options[:model].to_s)
+            @remixee    = DataMapper::Inflector.tableize(demodulized).to_sym
+          end
+        end
+
         attr_reader :model
         attr_reader :options
         attr_reader :translation_model
         attr_reader :proxy
+        attr_reader :naming_convention
 
         def initialize(model, options)
-          @model       = model
-          @options     = {
-            :as                       => nil,
-            :model                    => "#{model}Translation",
-            :accept_nested_attributes => true
-          }.merge(options)
+          @model             = model
+          @options           = default_options.merge(options)
+          @naming_convention = NamingConvention.new(@model, @options)
         end
 
         def localize(&block)
-
-          fk_string          = DataMapper::Inflector.foreign_key(model.name)
-          remixer_fk         = fk_string.to_sym
-          remixer            = fk_string[0, fk_string.rindex('_id')].to_sym
-          demodulized        = DataMapper::Inflector.demodulize(options[:model].to_s)
-          remixee            = DataMapper::Inflector.tableize(demodulized).to_sym
-
-          generate_translation_model(remixer, remixee, remixer_fk, &block)
+          generate_translation_model(&block)
 
           @translation_model = DataMapper::Inflector.constantize(options[:model])
           @proxy             = I18n::Model::Proxy.new(model, translation_model)
 
-          generate_accessor_aliases(remixee, options[:accepts_nested_attributes])
+          generate_accessor_aliases(options[:accepts_nested_attributes])
           generate_property_readers
 
           self
         end
 
-        def generate_translation_model(remixer, remixee, remixer_fk, &block)
+        def generate_translation_model(&block)
+          nc = naming_convention # make nc available in the current binding
+
           model.remix model.n, Translation,
             :as => options[:as],
             :model => options[:model]
 
           model.has model.n, :locales, DataMapper::I18n::Locale,
-            :through => remixee,
+            :through => nc.remixee,
             :constraint => :destroy
 
           model.enhance :translation do
 
-            belongs_to remixer,
+            belongs_to nc.remixer,
               :unique_index => :unique_locales
 
             belongs_to :locale, DataMapper::I18n::Locale,
@@ -104,23 +112,25 @@ module DataMapper
               :child_repository_name  => self.repository_name,
               :unique_index           => :unique_locales
 
-            validates_uniqueness_of :locale_id, :scope => remixer_fk
+            validates_uniqueness_of :locale_id, :scope => nc.remixer_fk
 
             class_eval &block
           end
           self
         end
 
-        def generate_accessor_aliases(remixee, nested_accessors)
+        def generate_accessor_aliases(nested_accessors)
+          nc = naming_convention # make nc available in the current binding
+
           model.class_eval do
             extend  I18n::Model::API
             include I18n::Resource::API
-            alias_method :translations, remixee
+            alias_method :translations, nc.remixee
 
             if nested_accessors
-              remixee_attributes = :"#{remixee}_atttributes"
+              remixee_attributes = :"#{nc.remixee}_atttributes"
 
-              accepts_nested_attributes_for remixee.to_sym
+              accepts_nested_attributes_for nc.remixee.to_sym
               alias_method :translations_attributes, remixee_attributes
             end
           end
@@ -138,13 +148,20 @@ module DataMapper
           self
         end
 
+        def default_options
+          {
+            :as    => nil,
+            :model => "#{model}Translation",
+            :accept_nested_attributes => true
+          }
+        end
+
       end # class Localizer
 
       def is_localizable(options = {}, &block)
         localizer = Localizer.new(self, options)
         localizer.localize(&block)
         @i18n = localizer.proxy
-
         self
       end
 
