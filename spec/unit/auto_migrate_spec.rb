cc1c6006fdb12240159daab08c53c4b58c664bbf
snusnu <gamsnjaga@gmail.com>
2011-09-30 00:28:23 +0200

    Make auto_migrate! specs work with dm-constraints

--- spec/unit/auto_migrate_spec.rb
+++ spec/unit/auto_migrate_spec.rb
@@ -2,12 +2,8 @@ require 'spec_helper'
 
 describe "DataMapper.auto_migrate!" do
 
-  it "should allow to automigrate the resource to be translated" do
-    lambda { Item.auto_migrate! }.should_not raise_error
-  end
-
-  it "should allow to automigrate the resource where translations will be stored" do
-    lambda { ItemTranslation.auto_migrate! }.should_not raise_error
+  it "should not raise errors" do
+    lambda { DataMapper.finalize.auto_migrate! }.should_not raise_error
   end
 
 end
